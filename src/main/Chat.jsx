03b6c6b2f4abf2f56c7c7c8d04ba079ba81d27b6
Kiet Ho <hoakiet98@gmail.com>
2021-01-11 15:49:11 -0500

    added chat UI demo

---
+++ src/main/Chat.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { ChatFeed, Message } from 'react-chat-ui';
+
+const users = {
+  You: 'You',
+  Mark: 'Mark'
+};
+
+class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [
+        new Message({ id: 'Mark', message: 'Hello Kiet!', senderName: 'Mark' })
+      ],
+      curr_user: 0,
+    };
+  }
+
+  onPress(user) {
+    this.setState({ curr_user: user });
+  }
+
+  onMessageSubmit(e) {
+    const input = this.message;
+    e.preventDefault();
+    if (!input.value) {
+      return false;
+    }
+    this.pushMessage(this.state.curr_user, input.value);
+    input.value = '';
+    return true;
+  }
+
+  pushMessage(recipient, message) {
+    const prevState = this.state;
+    const newMessage = new Message({
+      id: recipient,
+      message,
+      senderName: users[recipient],
+    });
+    prevState.messages.push(newMessage);
+    this.setState(this.state);
+  }
+
+  render() {
+    return (
+      <div className="container" >
+        <div className="chatfeed-wrapper">
+          <ChatFeed
+            maxHeight={250}
+            messages={this.state.messages} // Boolean: list of message objects
+            showSenderName
+          />
+
+          <form onSubmit={e => this.onMessageSubmit(e)}>
+            <input
+              ref={m => {
+                this.message = m;
+              }}
+              placeholder="Type a message..."
+              className="message-input"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Chat
--- src/main/MessagesPage.tsx
+++ src/main/MessagesPage.tsx
@@ -1,7 +1,9 @@
+import Chat from './Chat'
 function MessagesPage(){
     return (
         <div>
             <h1>Messages</h1>
+            <Chat/>
         </div>
     )
 }
